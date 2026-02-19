ac30e0c07af3469c9c3dfc9104e8c87d6a9e52b7
ReaQwQ <152592769+ReaQwQ@users.noreply.github.com>
2026-02-19 15:29:08 +0900

    Update AttackSyncMixin.java

--- src/main/java/com/reaqwq/packetoptimizer/mixin/combat/AttackSyncMixin.java
+++ src/main/java/com/reaqwq/packetoptimizer/mixin/combat/AttackSyncMixin.java
@@ -2,23 +2,25 @@ package com.reaqwq.packetoptimizer.mixin.combat;
 
 import com.reaqwq.packetoptimizer.config.PacketOptimizerConfig;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerEntity.class)
+@Mixin(PlayerEntity.class)
 public class AttackSyncMixin {
     @Inject(method = "attack", at = @At("HEAD"))
     private void onAttack(Entity target, CallbackInfo ci) {
         if (PacketOptimizerConfig.getInstance().crystalOptimizer && target instanceof EndCrystalEntity) {
-            target.discard();
-            MinecraftClient client = MinecraftClient.getInstance();
-            if (client.getNetworkHandler() != null) {
-                client.getNetworkHandler().getConnection().flush();
+            if (target.getWorld().isClient) {
+                target.discard();
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.getNetworkHandler() != null && client.getNetworkHandler().getConnection() != null) {
+                    client.getNetworkHandler().getConnection().flush();
+                }
             }
         }
     }
