5a1ca3e203ce8b736f811895500c636e3c8e4b59
ReaQwQ <152592769+ReaQwQ@users.noreply.github.com>
2026-02-19 16:46:16 +0900

    Update EarlyDecoderMixin.java

--- src/main/java/com/reaqwq/packetoptimizer/mixin/receive/EarlyDecoderMixin.java
+++ src/main/java/com/reaqwq/packetoptimizer/mixin/receive/EarlyDecoderMixin.java
@@ -2,22 +2,26 @@ package com.reaqwq.packetoptimizer.mixin.receive;
 
 import com.reaqwq.packetoptimizer.config.PacketOptimizerConfig;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.EntityPositionS2CPacket;
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 import io.netty.channel.ChannelHandlerContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.s2c.play.EntityPositionS2CPacket;
-import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 
 @Mixin(ClientConnection.class)
 public class EarlyDecoderMixin {
-    @Inject(method = "channelRead", at = @At("HEAD"))
-    private void onChannelRead(ChannelHandlerContext ctx, Object msg, CallbackInfo ci) {
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    private void onChannelRead0(ChannelHandlerContext ctx, Packet<?> packet, CallbackInfo ci) {
         if (PacketOptimizerConfig.getInstance().earlyDecoder) {
-            if (msg instanceof ExplosionS2CPacket || msg instanceof EntityPositionS2CPacket) {
-                ctx.fireChannelRead(msg);
+            if (packet instanceof ExplosionS2CPacket explosion) {
+                com.reaqwq.packetoptimizer.PacketOptimizer.getInstance().playExplosionEffects(explosion);
+                ctx.fireChannelRead(packet);
+                ci.cancel();
+            } else if (packet instanceof EntityPositionS2CPacket) {
+                ctx.fireChannelRead(packet);
                 ci.cancel();
             }
         }
